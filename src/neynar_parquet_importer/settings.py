e153d7e91782551e46a2cf0874e6c3d293fe7b12
Bryan Stitt <bryan@neynar.com>
2024-12-09 12:28:06 -0800

    cache dogstatsd differently

--- src/neynar_parquet_importer/settings.py
+++ src/neynar_parquet_importer/settings.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import datadog
 from datadog.dogstatsd import DogStatsd
 from pathlib import Path
@@ -26,19 +27,18 @@ class Settings(BaseSettings):
     s3_pool_size: int = 50
     target_name: str = "unknown"
 
+    @lru_cache(maxsize=1)
     def dogstatsd(self):
-        if self._dogstatsd is None:
-            if self.datadog_enabled:
-                datadog.initialize(
-                    statsd_constant_tags=[
-                        f"target:{self.target_name}",
-                        f"npe_version:{self.npe_version}-{self.incremental_duration}",
-                        f"parquet_db:{self.parquet_s3_database}",
-                        f"parquet_schema:{self.parquet_s3_schema}",
-                    ],
-                )
-            self._dogstatsd = DogStatsd()
-        return self._dogstatsd
+        if self.datadog_enabled:
+            datadog.initialize(
+                statsd_constant_tags=[
+                    f"target:{self.target_name}",
+                    f"npe_version:{self.npe_version}-{self.incremental_duration}",
+                    f"parquet_db:{self.parquet_s3_database}",
+                    f"parquet_schema:{self.parquet_s3_schema}",
+                ],
+            )
+        return DogStatsd()
 
     def parquet_s3_prefix(self):
         prefix = (
