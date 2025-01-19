0f68080a0cde001122362ac1daeef601767517af
Bryan Stitt <bryan@neynar.com>
2025-01-18 20:44:09 -0800

    better warning

--- schema/023_00_view_profile_with_addresses.sql
+++ schema/023_00_view_profile_with_addresses.sql
@@ -1,3 +1,37 @@
--- TODO: make sure nindexer profiles and verifications exist
--- TODO: create a view if it doesn't already exist
 -- TODO: make the migrations code able to handle views
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.views WHERE table_name = 'combined_account_profiles') THEN
+        CREATE VIEW profile_with_verifications AS
+        SELECT 
+            p.id AS profile_id,
+            p.created_at AS profile_created_at,
+            p.updated_at AS profile_updated_at,
+            p.deleted_at AS profile_deleted_at,
+            p.fid AS profile_fid,
+            p.bio,
+            p.pfp_url,
+            p."url",
+            p.username AS profile_username,
+            p.display_name,
+            p."location",
+            p.latitude,
+            p.longitude,
+            (
+                SELECT jsonb_agg(
+                    jsonb_build_object(
+                        'platform', av.platform,
+                        'platform_id', av.platform_id,
+                        'platform_username', av.platform_username,
+                        'verified_at', av.verified_at
+                    )
+                )
+                FROM account_verifications av
+                WHERE av.fid = p.fid
+                AND av.deleted_at IS NULL
+            ) AS verifications
+        FROM 
+            profiles p;
+    END IF;
+END $$;
