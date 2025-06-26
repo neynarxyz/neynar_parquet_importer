c59fb4a38f11f5e43593737274687403c401ce4b
topocount <17910833+topocount@users.noreply.github.com>
2025-06-26 17:02:55 -0500

    fix add app fids to v3 reactions table instead of v2

--- schema/026_00_public-postgres_nindexer_reactions.sql
+++ schema/026_00_public-postgres_nindexer_reactions.sql
@@ -21,6 +21,30 @@ BEGIN
         CREATE INDEX IF NOT EXISTS idx_reactions_upsert
         ON ${POSTGRES_SCHEMA}.reactions (id, updated_at);
     END IF;
+
+    -- add creator_app_fid and deleter_app_fid columns if they don't already exist
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'reactions'
+        AND table_schema = '${POSTGRES_SCHEMA}'
+        AND column_name = 'creator_app_fid'
+    ) THEN
+        ALTER TABLE ${POSTGRES_SCHEMA}.reactions
+        ADD COLUMN creator_app_fid bigint;
+    END IF;
+
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'reactions'
+        AND table_schema = '${POSTGRES_SCHEMA}'
+        AND column_name = 'deleter_app_fid'
+    ) THEN
+        ALTER TABLE ${POSTGRES_SCHEMA}.reactions
+        ADD COLUMN deleter_app_fid bigint;
+    END IF;
+
 END $$;
 
 
